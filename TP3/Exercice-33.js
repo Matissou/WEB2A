f9f42a89f0ebcf7ddaf8cb81022be6a6aa1031df
matissou <matisse.landais@gmail.com>
2019-01-30 18:43:32 +0100

    Commentaires exo 3

--- TP3/Exercice-33.js
+++ TP3/Exercice-33.js
@@ -1,24 +1,35 @@
+// Variables globales pour partager entre les fonctions
 var defaultDisplay;
 var checkbox;
 
 window.onload = function(){
     
-    //Valeur par défaut : visible
+    initialize();
+}
+
+function initialize(){
+    //La valeur par défaut (qui peut être block, inline ...)
     defaultDisplay = document.querySelector('#menu').style.display;
-    
-    // Cacher de base le menu
+
+    //On cache le menu
     document.querySelector("#menu").style.display="none";
+
+    //On ajoute un ecouteur sur le bouton pour trigger la fonction hide
+    // quand un changement se produit
     checkbox = document.querySelector("#showMenu");
     checkbox.addEventListener("change",hide);
 }
 
 function hide(){
 
+    //On récupère la valeur actuelle de la checkbox
     var checkedValue = document.querySelector('#showMenu').checked;
     if(checkedValue==false)
     {
+        //On cache
         document.getElementById('menu').style.display = "none";
     }else{
+        //Ou sinon on reprend la disposition de base
         document.getElementById('menu').style.display = defaultDisplay;
     }
 }
