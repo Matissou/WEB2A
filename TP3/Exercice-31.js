abc8694968a021ec5cb4f15101c36bce6ab1ecc9
matissou <matisse.landais@gmail.com>
2019-01-30 14:35:45 +0100

    Exo 2 ok, commentaires dans Exo 1 et 2

--- TP3/Exercice-31.js
+++ TP3/Exercice-31.js
@@ -8,10 +8,12 @@ window.onload = function(){
 function versionSimple(){
         //Version facile 
 
+        //Création de deux textNode que l'on assignera aux li
         var one = document.createTextNode("One");
         var two = document.createTextNode("Two");
         var three = document.createTextNode("Three");
     
+        // Creation des li et ajout des text nodes précédant
         var li1 = document.createElement("li");
         li1.appendChild(one);
     
@@ -21,11 +23,14 @@ function versionSimple(){
         var li3 = document.createElement("li");
         li3.appendChild(three);
     
+        //Création du ul
         var ul = document.createElement("ul");
         ul.appendChild(li1);
         ul.appendChild(li2);
         ul.appendChild(li3);
     
+        //On ajoute le tout au body/
+        // le [0] permet de selectionner body car la fonction renvoie un tableau
         test = document.getElementsByTagName("body")[0];
         test.appendChild(ul);
 }
@@ -37,14 +42,16 @@ function versionPropre()
     var ott = ["One", "Two", "Three"];
     var ul = document.createElement("ul");
     var lli;
+    //On parcours le tableau de li pour diminuer le nombre de lignes
     for(i=0; i<ott.length; i++)
     {
-       // document.body.appendChild("p").appendChild(ott[i]);
+        //On ajoute le textNode au li; et le li au ul
         lli = document.createTextNode(ott[i]);
         li = document.createElement("li");
         li.appendChild(lli);
         ul.appendChild(li);
     }
+    //Puis on ajoute le tout au body
     document.getElementsByTagName("body")[0].appendChild(ul);
 
 }
