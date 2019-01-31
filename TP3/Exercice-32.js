d3665c0596156a841300ee7d7c0124312d62d1bf
matissou <matisse.landais@gmail.com>
2019-01-31 08:38:47 +0100

    Plus de ;, ajout fichiers TP3.2

--- TP3/Exercice-32.js
+++ TP3/Exercice-32.js
@@ -2,44 +2,44 @@
 window.onload = function(){
 
     // On récupère la couleur par défaut du titre
-    var defaultColor = document.querySelector('#introduction').style.color;
+    var defaultColor = document.querySelector('#introduction').style.color
 
     //Bouton 1 2 et 3 
-    var act1 = document.getElementById('b1');
-    var act2 = document.getElementById('b2');
-    var act3 = document.getElementById('b3');
+    var act1 = document.getElementById('b1')
+    var act2 = document.getElementById('b2')
+    var act3 = document.getElementById('b3')
 
     //La fonction suivante s'exécute lors du clique sur le boutton
     act1.onclick = function changeBG(){
-        document.getElementById('contents').style.backgroundColor = "yellow";
+        document.getElementById('contents').style.backgroundColor = "yellow"
     }
 
     //Action 2
     act2.onclick = function alternateTitleColor(){
         // Nouvelle façon de selectionner un element DOM
         //(d'après william, elementByTagName est pourris)
-        const doc = document.querySelector('#introduction');
+        const doc = document.querySelector('#introduction')
         // On accède au style et à la propriété couleur du h1
-        couleur = doc.style.color;
+        couleur = doc.style.color
         
         //On change la couleur selon la règle
         if(couleur == defaultColor){
-            couleur = "red";
+            couleur = "red"
         }else{
-            couleur = defaultColor;
+            couleur = defaultColor
         }
-        doc.style.color=couleur;
+        doc.style.color=couleur
     }
 
     //Action 3
     //Tableau des paragraphes
-    var plist = document.getElementsByTagName("p");
+    var plist = document.getElementsByTagName("p")
     //Dernier indice du paragraphe changé
-    var plistind = 0;
+    var plistind = 0
     b3.onclick =  function(){
         if(plistind < plist.length){
-            plist[plistind].style.fontStyle = "italic";
-            plistind ++;
+            plist[plistind].style.fontStyle = "italic"
+            plistind ++
         }
     }
 }
--- TP3/Exercice-33.js
+++ TP3/Exercice-33.js
@@ -1,46 +1,45 @@
 /* Ce code s'execute bien sur Google Chrome v 71.0 */
 
 // Variables globales pour partager entre les fonctions
-var defaultDisplay;
-var checkbox;
-var selector;
+var defaultDisplay
+var checkbox
+var selector
 
 window.onload = function(){
-    
-    initialize();
+    initialize()
 }
 
 function initialize(){
     //La valeur par défaut (qui peut être block, inline ...)
-    defaultDisplay = document.querySelector('#menu').style.display;
+    defaultDisplay = document.querySelector('#menu').style.display
 
     //On cache le menu
-    document.querySelector("#menu").style.display="none";
+    document.querySelector("#menu").style.display="none"
 
     //On ajoute un ecouteur sur le bouton pour trigger la fonction hide
     // quand un changement se produit
-    checkbox = document.querySelector("#showMenu");
-    checkbox.addEventListener("change",hide);
+    checkbox = document.querySelector("#showMenu")
+    checkbox.addEventListener("change",hide)
 
     //Idem pour réagir au changement dans la liste déroulante
-    selector = document.getElementById("ltheme");
-    selector.addEventListener("change", switchTheme);
+    selector = document.getElementById("ltheme")
+    selector.addEventListener("change", switchTheme)
 
     //Par defaut, theme 1 et theme 1 -> checkBox cachée donc on demande une fois a switchTheme de travailler
-    switchTheme();
+    switchTheme()
 }
 
 function hide(){
 
     //On récupère la valeur actuelle de la checkbox
-    var checkedValue = document.querySelector('#showMenu').checked;
+    var checkedValue = document.querySelector('#showMenu').checked
     if(checkedValue==false)
     {
         //On cache
-        document.getElementById('menu').style.display = "none";
+        document.getElementById('menu').style.display = "none"
     }else{
         //Ou sinon on reprend la disposition de base
-        document.getElementById('menu').style.display = defaultDisplay;
+        document.getElementById('menu').style.display = defaultDisplay
     }
 }
 
@@ -50,9 +49,9 @@ function switchTheme(){
     //Récupération de l'index de l'élément selectionné. Commence a 0 donc +1 pour changer l'attribut class
     var index = selector.selectedIndex+1;
     if (index == 1){
-        document.getElementById("showMenu").parentElement.style.display = "none";
+        document.getElementById("showMenu").parentElement.style.display = "none"
     }else{
-        document.getElementById("showMenu").parentElement.style.display = "";
+        document.getElementById("showMenu").parentElement.style.display = ""
     }
-    document.body.setAttribute("class", 'theme'+index);
+    document.body.setAttribute("class", 'theme'+index)
 }
