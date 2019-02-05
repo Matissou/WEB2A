c153de32b919f88d38b0e6e21fa4b8e789253e96
loicrosso <loic.rosso@grenoble-inp.org>
2019-02-05 15:20:55 +0100

    ajout fonction nombre

--- TP4/Exercice-41.js
+++ TP4/Exercice-41.js
@@ -1,5 +1,6 @@
 window.addEventListener('load', cacherVide , false);
 window.addEventListener('load', colorerLignes , false);
+window.addEventListener('load', nombres , false);
 
 // Fonction qui cache les div correspondant aux tbody vides de la page
 function cacherVide(){
@@ -20,4 +21,10 @@ function colorerLignes(){
     $("tbody tr:nth-child(3n+2)").addClass("lig2")
     // Ajoute la class lig1 tous les 3 tr en partant de 3
     $("tbody tr:nth-child(3n+3)").addClass("lig3")
+}
+
+// Applique la class nombre a chaque nombre de la ligne
+function nombres(){
+    $("body tr td:nth-child(2)").addClass("nombre")
+    $("body tr td:nth-child(3)").addClass("nombre")
 }
