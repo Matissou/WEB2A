9d9feb8fb3f54d0789758c69cc6d670ead5f4792
matissou <matisse.landais@gmail.com>
2019-02-05 15:30:35 +0100

    tp4 ex 36

--- TP3/Exercice-36.js
+++ TP3/Exercice-36.js
@@ -1,16 +1,18 @@
+const valeurCellule = (tr, idx) => tr.children[idx].innerText || tr.children[idx].textContent;    
+const comparer = (idx, asc) => (a, b) => ((v1, v2) => 
+v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2) ? v1 - v2 : v1.toString().localeCompare(v2)
+)(valeurCellule(asc ? a : b, idx), valeurCellule(asc ? b : a, idx));
+
 window.onload = function(){
-    let tableau = document.getElementById('T-1')
-    let nbHeaders = tableau.getElementsByTagName('th').length
-
-    let arrH = new Array()
-    for(i=0; i<nbHeaders; i++)
-    {
-        let h = document.getElementsByTagName('th')[i]
-        h.addEventListener("click",sortColumn(i))
-        
-        
-    }
-
-
+    let ecouteurs = document.querySelectorAll('th').forEach(th => th.addEventListener('click',function(){sortColumnH(th);},false));
 }
+
+function sortColumnH(th)
+{
+    const table = th.closest('table');  
+    var delF = [].slice.call(table.querySelectorAll('tr:nth-child(n+1)'),1);
+    let ligneParente = Array.from(th.parentNode.children).indexOf(th)
     
+    let tri = Array.from(delF).sort(comparer(ligneParente, this.asc = !this.asc))
+    tri.forEach(tr => table.appendChild(tr) );
+}
